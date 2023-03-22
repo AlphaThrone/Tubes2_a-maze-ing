7b05bff4f31d21a002ecf4b77dd2b43105a851e1
goodgirlwannabe <13521006@mahasiswa.itb.ac.id>
2023-03-22 14:28:37 +0700

    added gui

---
+++ TreasureHunt.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace TreasureHunt
+{
+    partial class TreasureHunt
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TreasureHunt));
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            button4 = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.Rectangle_6;
+            pictureBox1.Location = new Point(1049, 32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(900, 900);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            pictureBox1.UseWaitCursor = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__4__3;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(38, 565);
+            button1.Name = "button1";
+            button1.Size = new Size(247, 133);
+            button1.TabIndex = 7;
+            button1.UseVisualStyleBackColor = false;
+            button1.UseWaitCursor = true;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__5__3;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(402, 565);
+            button2.Name = "button2";
+            button2.Size = new Size(242, 135);
+            button2.TabIndex = 8;
+            button2.UseVisualStyleBackColor = false;
+            button2.UseWaitCursor = true;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__11__2;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(747, 565);
+            button3.Name = "button3";
+            button3.Size = new Size(243, 133);
+            button3.TabIndex = 9;
+            button3.UseVisualStyleBackColor = false;
+            button3.UseWaitCursor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__12__2;
+            pictureBox2.Location = new Point(58, 841);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(177, 74);
+            pictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox2.TabIndex = 10;
+            pictureBox2.TabStop = false;
+            pictureBox2.UseWaitCursor = true;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__13__2;
+            pictureBox3.Location = new Point(58, 1007);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(177, 65);
+            pictureBox3.TabIndex = 11;
+            pictureBox3.TabStop = false;
+            pictureBox3.UseWaitCursor = true;
+            pictureBox3.Click += pictureBox3_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__14__2;
+            pictureBox4.Location = new Point(536, 999);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(195, 84);
+            pictureBox4.TabIndex = 12;
+            pictureBox4.TabStop = false;
+            pictureBox4.UseWaitCursor = true;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackColor = Color.Transparent;
+            pictureBox5.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__15__2;
+            pictureBox5.Location = new Point(1049, 982);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(210, 101);
+            pictureBox5.TabIndex = 13;
+            pictureBox5.TabStop = false;
+            pictureBox5.UseWaitCursor = true;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.BackgroundImage = Properties.Resources.Spongebob_Font_Generator__2__3;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(1700, 967);
+            button4.Name = "button4";
+            button4.Size = new Size(249, 127);
+            button4.TabIndex = 14;
+            button4.UseVisualStyleBackColor = false;
+            button4.UseWaitCursor = true;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(28, 28);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.Yellow;
+            label1.Location = new Point(254, 857);
+            label1.Name = "label1";
+            label1.Size = new Size(315, 38);
+            label1.TabIndex = 18;
+            label1.Text = "D D D R L U U U L U D ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Gill Sans Ultra Bold Condensed", 20.1428585F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.Yellow;
+            label2.Location = new Point(51, 413);
+            label2.Name = "label2";
+            label2.Size = new Size(629, 67);
+            label2.TabIndex = 19;
+            label2.Text = "BFS is bla bla bla bla bla bla";
+            label2.Click += label2_Click;
+            // 
+            // TreasureHunt
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1976, 1136);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            ForeColor = Color.Transparent;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            HelpButton = true;
+            Name = "TreasureHunt";
+            SizeGripStyle = SizeGripStyle.Show;
+            Text = "Treasure Hunt";
+            TransparencyKey = SystemColors.Window;
+            UseWaitCursor = true;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Button button4;
+        private ContextMenuStrip contextMenuStrip1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ TreasureHunt.cs
@@ -0,0 +1,50 @@
+namespace TreasureHunt
+{
+    public partial class TreasureHunt : Form
+    {
+        public TreasureHunt()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
